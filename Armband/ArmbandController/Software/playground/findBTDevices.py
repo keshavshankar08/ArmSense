ee165e5726aafb7eb695f3f32a363ffdcc68eccb
Keshav Shankar <keshavshankar08@gmail.com>
2024-12-06 18:26:00 -0500

    get rid of playground

--- Armband/ArmbandController/Software/playground/findBTDevices.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import BleakScanner
-
-async def find_devices():
-    devices = await BleakScanner.discover()
-    for device in devices:
-        print(f"Found device: {device.name}, Address: {device.address}")
-
-asyncio.run(find_devices())
